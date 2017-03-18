13331cc680c16ccbc65fc5e487b33ef06c8e7b24
Wolfy <s.leheup@gmail.com>
2017-03-18 18:31:21 +0100

    Angular Quest

--- js/app.js
+++ js/app.js
@@ -1,2 +1,20 @@
-angular.module('app', [])
-    .controller('mainController', mainController);
+// angular.module('app', [])
+//     .controller('mainController', mainController);
+
+
+    var helloApp = angular.module('helloApp', ['ngRoute', 'helloAppControllers']);
+
+helloApp.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.
+    when('/', {
+        templateUrl: 'hello.html',
+        controller: 'helloCtrl'
+    }).
+    when('/people', {
+        templateUrl: 'people.html',
+        controller: 'peopleCtrl'
+    }).
+    otherwise({
+        redirectTo: '/'
+    });
+}]);
--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,4 +1,25 @@
 // MAIN CONTROLLER
-function mainController($scope) {
-    
-}
+// function mainController($scope) {
+//
+// }
+
+var helloAppControllers = angular.module('helloAppControllers', []);
+
+helloAppControllers.controller('helloCtrl', ['$scope', function($scope) {
+    // $scope.name = ')world';
+}]);
+
+helloAppControllers.controller('peopleCtrl', ['$scope', function($scope) {
+    $scope.tab = [];
+    var maLigne;
+
+    $scope.add = function() {
+        maLigne = {
+            name: $scope.name,
+            firstname: $scope.firstname,
+            mail: $scope.mail
+        };
+        $scope.tab.push(maLigne);
+        console.log($scope.tab);
+    };
+}]);
